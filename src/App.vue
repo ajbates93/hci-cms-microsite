657ba0e0aa5a2c9c0178c8653bcb8c3e3dfd4d7e
Alex Bates <ajbates93@gmail.com>
2022-09-08 07:29:32 +0100

    feat: enable different bg colours for dividers

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,19 @@ import Banner from "./components/Banner.vue";
 import Header from "./components/Header.vue";
 import DarkDivider from "./components/DarkDivider.vue";
 import { ref } from 'vue'
-import { useIntersectionObserver } from "@vueuse/core";
+import { useElementVisibility } from "@vueuse/core";
 
 const target = ref<HTMLDivElement>()
-const targetIsVisible = ref(false)
-
-const { stop } = useIntersectionObserver(
-  target, 
-  ([{ isIntersecting }], observerElement) => {
-    targetIsVisible.value = isIntersecting
-  })
+const targetIsVisible = useElementVisibility(target)
 
 </script>
 
 <template>
   <Header />
   <Banner />
-  <main id="main" role="main" bg-white min-h-300 text-center>
+  <main id="main" role="main" dark:bg-white bg-slate-3 transition min-h-300 text-center>
     <DarkDivider />
-    <div ref="target" v-if="targetIsVisible">
+    <div ref="target" :class="targetIsVisible ? ['active'] : ''">
       <div>Fully Bespoke</div>
       <div>Membership Management</div>
       <div>Daily & Weekly Backups</div>
